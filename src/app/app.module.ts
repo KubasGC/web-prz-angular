c881cfe1c6bada3ac0276a0401c2d77c221bb1bc
Kubas <kubasgc@live.com>
2018-10-11 13:36:58 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import {MzNavbarModule, MzParallaxModule} from 'ngx-materialize';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MzNavbarModule,
+    MzParallaxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
